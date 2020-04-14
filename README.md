9fd9cc02fe5a0e57d790b03572f0d7bcd990662d
Andrej Karpathy <andrej.karpathy@gmail.com>
2020-04-14 00:25:30 -0700

    fix a bug, end up in a much better spot overall. thank you @sinjax and @evcu for help and contributions on which i drew for this commit

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 ![awww](puppy.jpg)
 
-A tiny Autograd engine (with a bite! :D). Implements backpropagation over a dynamically built DAG and a small neural networks library on top of it with a PyTorch-like API. Both are currently about 50 lines of code each.
+A tiny Autograd engine (with a bite! :D). Implements backpropagation (reverse-mode autodiff) over a dynamically built DAG and a small neural networks library on top of it with a PyTorch-like API. Both are currently about 50 lines of code each.
 
-The amusing part is that the DAG only allows individual scalar values, so e.g. we chop up each neuron into all of its individual tiny adds and multiplies. In particular, the current library only supports scalars and three operations over them: (+,*,relu), but these are actually enough to build up an entire deep neural net doing binary classification as the demo notebook shows. It's just a lot of nodes :)
+**super lite**. The DAG only allows individual scalar values, so e.g. we chop up each neuron into all of its individual tiny adds and multiplies. In particular, the current library only supports scalars and three operations over them: (+,*,relu), but in fact these are enough to build up an entire deep neural net doing binary classification, as the demo notebook shows.
 
 Potentially useful for educational purposes. See the notebook for demo.
 
--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,50 +2,67 @@
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data):
+    def __init__(self, data, _children=()):
         self.data = data
         self.grad = 0
-        self.backward = lambda: None
+        # internal variables used for autograd graph construction
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._visited = False
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other))
 
-        def backward():
+        def _backward():
             self.grad += out.grad
             other.grad += out.grad
-            self.backward()
-            other.backward()
-        out.backward = backward
+        out._backward = _backward
 
         return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other))
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,))
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+
+        # topological order all of the children in the graph
+        topo = []
+        def build_topo(v):
+            if not v._visited:
+                v._visited = True
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
 
     def __radd__(self, other):
         return self.__add__(other)
 
-    def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data)
-
-        def backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-            self.backward()
-            other.backward()
-        out.backward = backward
-
-        return out
-
     def __rmul__(self, other):
         return self.__mul__(other)
-
-    def relu(self):
-        out = Value(0 if self.data < 0 else self.data)
-        def backward():
-            self.grad += (out.data > 0) * out.grad
-            self.backward()
-        out.backward = backward
-        return out
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
