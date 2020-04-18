dfd0c0571d07bfd7be09820c1cc229d28294907d
fcakyon <fcakyon@gmail.com>
2020-04-19 00:33:26 +0300

    add github actions continious integration

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,67 +1,75 @@
+import unittest
 import torch
 from micrograd.engine import Value
 
-def test_sanity_check():
 
-    x = Value(-4.0)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xmg, ymg = x, y
+class TestEngine(unittest.TestCase):
 
-    x = torch.Tensor([-4.0]).double()
-    x.requires_grad = True
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xpt, ypt = x, y
+    def test_sanity_check(self):
 
-    # forward pass went well
-    assert ymg.data == ypt.data.item()
-    # backward pass went well
-    assert xmg.grad == xpt.grad.item()
+        x = Value(-4.0)
+        z = 2 * x + 2 + x
+        q = z.relu() + z * x
+        h = (z * z).relu()
+        y = h + q + q * x
+        y.backward()
+        xmg, ymg = x, y
 
-def test_more_ops():
+        x = torch.Tensor([-4.0]).double()
+        x.requires_grad = True
+        z = 2 * x + 2 + x
+        q = z.relu() + z * x
+        h = (z * z).relu()
+        y = h + q + q * x
+        y.backward()
+        xpt, ypt = x, y
 
-    a = Value(-4.0)
-    b = Value(2.0)
-    c = a + b
-    d = a * b + b**3
-    c += c + 1
-    c += 1 + c + (-a)
-    d += d * 2 + (b + a).relu()
-    d += 3 * d + (b - a).relu()
-    e = c - d
-    f = e**2
-    g = f / 2.0
-    g += 10.0 / f
-    g.backward()
-    amg, bmg, gmg = a, b, g
+        # forward pass went well
+        self.assertEqual(ymg.data, ypt.data.item())
+        # backward pass went well
+        self.assertEqual(xmg.grad, xpt.grad.item())
 
-    a = torch.Tensor([-4.0]).double()
-    b = torch.Tensor([2.0]).double()
-    a.requires_grad = True
-    b.requires_grad = True
-    c = a + b
-    d = a * b + b**3
-    c = c + c + 1
-    c = c + 1 + c + (-a)
-    d = d + d * 2 + (b + a).relu()
-    d = d + 3 * d + (b - a).relu()
-    e = c - d
-    f = e**2
-    g = f / 2.0
-    g = g + 10.0 / f
-    g.backward()
-    apt, bpt, gpt = a, b, g
+    def test_more_ops(self):
 
-    tol = 1e-6
-    # forward pass went well
-    assert abs(gmg.data - gpt.data.item()) < tol
-    # backward pass went well
-    assert abs(amg.grad - apt.grad.item()) < tol
-    assert abs(bmg.grad - bpt.grad.item()) < tol
+        a = Value(-4.0)
+        b = Value(2.0)
+        c = a + b
+        d = a * b + b**3
+        c += c + 1
+        c += 1 + c + (-a)
+        d += d * 2 + (b + a).relu()
+        d += 3 * d + (b - a).relu()
+        e = c - d
+        f = e**2
+        g = f / 2.0
+        g += 10.0 / f
+        g.backward()
+        amg, bmg, gmg = a, b, g
+
+        a = torch.Tensor([-4.0]).double()
+        b = torch.Tensor([2.0]).double()
+        a.requires_grad = True
+        b.requires_grad = True
+        c = a + b
+        d = a * b + b**3
+        c = c + c + 1
+        c = c + 1 + c + (-a)
+        d = d + d * 2 + (b + a).relu()
+        d = d + 3 * d + (b - a).relu()
+        e = c - d
+        f = e**2
+        g = f / 2.0
+        g = g + 10.0 / f
+        g.backward()
+        apt, bpt, gpt = a, b, g
+
+        tol = 1e-6
+        # forward pass went well
+        self.assertEqual(abs(gmg.data - gpt.data.item()) < tol, True)
+        # backward pass went well
+        self.assertEqual(abs(amg.grad - apt.grad.item()) < tol, True)
+        self.assertEqual(abs(bmg.grad - bpt.grad.item()) < tol, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
