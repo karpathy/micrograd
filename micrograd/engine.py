d950e61b19639eed77ba6ab47f0debf713ba24eb
rhalbersma <rhalbersma@gmail.com>
2020-12-18 13:38:03 +0100

    rename _backward to gradient_fn and wrap into lambda taking self as argument

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,37 +2,37 @@
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, _children=(), _op='', requires_grad=False, _backward=None):
+    def __init__(self, data, _children=(), _op='', requires_grad=False, gradient_fn=None):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._prev = _children
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
         self.requires_grad = requires_grad
-        self._backward = _backward
+        self.gradient_fn = None if not gradient_fn else lambda: gradient_fn(self)
 
     def __add__(self, other):
         if not isinstance(other, Value): 
             other = Value(other)
         requires_grad = self.requires_grad or other.requires_grad
-        local_grad = None if not requires_grad else lambda _: (1, 1)
-        return Value(self.data + other.data, (self, other), '+', requires_grad, local_grad)
+        gradient_fn = None if not requires_grad else lambda _: (1, 1)
+        return Value(self.data + other.data, (self, other), '+', requires_grad, gradient_fn)
 
     def __mul__(self, other):
         if not isinstance(other, Value): 
             other = Value(other)
         requires_grad = self.requires_grad or other.requires_grad
-        local_grad = None if not requires_grad else lambda n: (n._prev[1].data, n._prev[0].data)
-        return Value(self.data * other.data, (self, other), '*', requires_grad, local_grad)
+        gradient_fn = None if not requires_grad else lambda v: (v._prev[1].data, v._prev[0].data)
+        return Value(self.data * other.data, (self, other), '*', requires_grad, gradient_fn)
 
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        local_grad = None if not self.requires_grad else lambda n: (other * n._prev[0].data**(other-1),)
-        return Value(self.data**other, (self,), f'**{other}', self.requires_grad, local_grad)
+        gradient_fn = None if not self.requires_grad else lambda v: (other * v._prev[0].data**(other-1),)
+        return Value(self.data**other, (self,), f'**{other}', self.requires_grad, gradient_fn)
 
     def relu(self):
-        local_grad = None if not self.requires_grad else lambda n: (n.data > 0,)
-        return Value(max(0, self.data), (self,), 'ReLU', self.requires_grad, local_grad)
+        gradient_fn = None if not self.requires_grad else lambda v: (v.data > 0,)
+        return Value(max(0, self.data), (self,), 'ReLU', self.requires_grad, gradient_fn)
 
     def _toposort(self):
         postorder = []
@@ -50,7 +50,7 @@ class Value:
         # go one variable at a time and apply the chain rule to get its gradient
         self.grad = 1
         for v in self._toposort():
-            for child, local_grad in zip(v._prev, v._backward(v)):
+            for child, local_grad in zip(v._prev, v.gradient_fn()):
                 if child.requires_grad:
                     child.grad += local_grad * v.grad
 
