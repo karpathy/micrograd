04d8e0c6d38b504ecc429966de3832a47e996d29
Kulwa-silya <kulwamalyango@gmail.com>
2022-08-29 18:34:38 +0300

    fixed initializer bug for class Value

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -3,7 +3,7 @@ class Value:
     """ stores a single scalar value and its gradient """
 
     def __init__(self, data, _children=(), _op=''):
-        self.data = data
+        self.data = data.data if isinstance(data, Value) else data 
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
