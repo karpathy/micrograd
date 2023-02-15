4785439970b55af373011eb9c3240825051ae52b
David Hidvegi <DavidHidvegi@users.noreply.github.com>
2023-02-15 22:05:21 +0100

    Potentially more elegant solution
    Initializing grad with 1 with might be hacky, but the backward propagation looks more elegant or a bit easier to understand in my opinion. :)

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -4,7 +4,7 @@ class Value:
 
     def __init__(self, data, _children=(), _op=''):
         self.data = data
-        self.grad = 0
+        self.grad = 1
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
@@ -52,22 +52,9 @@ class Value:
         return out
 
     def backward(self):
-
-        # topological order all of the children in the graph
-        topo = []
-        visited = set()
-        def build_topo(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v._prev:
-                    build_topo(child)
-                topo.append(v)
-        build_topo(self)
-
-        # go one variable at a time and apply the chain rule to get its gradient
-        self.grad = 1
-        for v in reversed(topo):
-            v._backward()
+        self._backward()
+        for child in list(self._prev):
+            child.backward()
 
     def __neg__(self): # -self
         return self * -1
