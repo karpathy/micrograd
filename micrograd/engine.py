edcde3dbd2ef9fe8fd26ed3e71ce63fc35836906
Farid Zakaria <fmzakari@google.com>
2024-01-20 23:16:12 +0000

    Minor fixes to engine.py for passing parent information for __pow__

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,4 +1,3 @@
-
 class Value:
     """ stores a single scalar value and its gradient """
 
@@ -34,7 +33,7 @@ class Value:
 
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data**other, (self,), f'**{other}')
+        out = Value(self.data**other, (self, Value(other)), f'**{other}')
 
         def _backward():
             self.grad += (other * self.data**(other-1)) * out.grad
