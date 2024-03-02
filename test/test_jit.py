d3e5e4d8111b3e08b401ba69a4d413b29cbe803d
Farid Zakaria <fmzakari@google.com>
2024-03-02 22:57:02 +0000

    Format test_jit.py

--- test/test_jit.py
+++ test/test_jit.py
@@ -4,8 +4,10 @@ import timeit
 from micrograd.engine import Value
 from micrograd.nn import Neuron, Layer, MLP
 from micrograd.jit import jit
+
 # helps investigate segmentation faults
 import faulthandler
+
 faulthandler.enable()
 
 
@@ -20,7 +22,7 @@ def test_value():
 
 def test_neuron():
     n = Neuron(nin=1, nonlin=False)
-    n.w = [2.]
+    n.w = [2.0]
     jn = jit(n)
     args = [10.0]
     assert math.isclose(n(args).data, jn(args), abs_tol=1e-04)
@@ -30,7 +32,7 @@ def test_layer():
     random.seed(10)
     l = Layer(nin=2, nout=1)
     jl = jit(l)
-    args = [-30., -20.]
+    args = [-30.0, -20.0]
     assert math.isclose(l(args).data, jl(args), abs_tol=1e-04)
 
 
@@ -38,7 +40,7 @@ def test_layer_multiple_out():
     random.seed(10)
     l = Layer(nin=2, nout=2)
     jl = jit(l)
-    args = [-30., -20.]
+    args = [-30.0, -20.0]
     for r, jr in zip(l(args), jl(args)):
         assert math.isclose(r.data, jr, abs_tol=1e-04)
 
@@ -47,7 +49,7 @@ def test_mlp():
     random.seed(10)
     nn = MLP(nin=2, nouts=[1])
     jnn = jit(nn)
-    args = [-30., -20.]
+    args = [-30.0, -20.0]
     assert math.isclose(nn(args).data, jnn(args), abs_tol=1e-04)
 
 
@@ -55,7 +57,7 @@ def test_mlp_complex():
     random.seed(10)
     nn = MLP(nin=2, nouts=[2, 1])
     jnn = jit(nn)
-    args = [-30., -20.]
+    args = [-30.0, -20.0]
     assert math.isclose(nn(args).data, jnn(args), abs_tol=1e-04)
 
 
@@ -63,7 +65,7 @@ def test_mlp_complex_multiple_out():
     random.seed(10)
     nn = MLP(nin=2, nouts=[2, 2])
     jnn = jit(nn)
-    args = [-30., -20.]
+    args = [-30.0, -20.0]
     for r, jr in zip(nn(args), jnn(args)):
         assert math.isclose(r.data, jr, abs_tol=1e-04)
 
@@ -79,6 +81,7 @@ def test_mlp_performance():
 
     def fast_inference():
         return jnn(args)
+
     slow_inference_time = timeit.timeit(slow_inference, number=1000)
     fast_inference_time = timeit.timeit(fast_inference, number=1000)
     print(f"\nslow: {slow_inference_time}\nfast: {fast_inference_time}")
